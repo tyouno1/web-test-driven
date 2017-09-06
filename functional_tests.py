f3b76cc68521da259d198c78fc3e3bc5ccd36730
tyouno1 <tyouno1@gmail.com>
2017-09-06 15:29:50 +0800

    add POST form to lists

--- functional_tests.py
+++ functional_tests.py
@@ -31,17 +31,32 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了"1: Buy peacok feathers"
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacok feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        #self.assertTrue(
+        #    any(row.text == '1: Buy peacok feathers' for row in rows),
+        #    "New to-do item did not appear in table -- its text was:\n%s" %(table.text,)
+        #)
 
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        
         # 页面中又显示了一个文本框，可以输入其他的代办事项
         # 她输入了"Use peacock feathers to make a fly"
         # 伊迪斯做事很有条理
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacok feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 页面再次刷新，清单中显示了两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly' ,
+            [row.text for row in rows]
+        )
+
+        ####
         self.fail('Finish the test!')
 
-        # 页面再次更新，她的清单中显示了这两个待办事项
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
